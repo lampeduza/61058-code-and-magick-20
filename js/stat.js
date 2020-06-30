8c0af1838a1f202ee778967c4f0e0f040cbe1943
lampeduza <lmdogg@mail.ru>
2020-07-01 00:21:40 +0700

    Добавил функцию рандомного добавления цвета

--- js/stat.js
+++ js/stat.js
@@ -42,9 +42,11 @@ window.renderStatistics = function (ctx, players, times) {
   }
 };
 
-var getColorByName = function (name) {
-  return name === 'Вы' ? 'rgba(255, 0, 0, 1)' : '#00f';
-}
+var getRandomColor = function () {
+  var saturation = Math.floor(Math.random() * 101);
+
+  return 'hsl(240, ' + saturation + '%, 50%)';
+};
 
 var renderHistogram = function (ctx, player, time, maxTime, index) {
 
@@ -52,132 +54,9 @@ var renderHistogram = function (ctx, player, time, maxTime, index) {
   var x = 140 + BAR_WIDTH * index * 2;
   var y = 90 + MAX_BAR_HEIGHT - barHeight;
 
-  ctx.fillStyle = getColorByName(player);
+  ctx.fillStyle = player === 'Вы' ? 'rgba(255, 0, 0, 1)' : getRandomColor();
   ctx.fillRect(x, y, BAR_WIDTH, barHeight);
-
   ctx.fillStyle = '#000';
   ctx.fillText(Math.round(time), x, y - GAP);
-
   ctx.fillText(player, x, 100 + MAX_BAR_HEIGHT + FONT_GAP);
 }
-/*
-
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP = 10; // отступ
-var BAR_GAP = 50; // размер отступа
-var BAR_WIDTH = 50; // ширина колонки
-var TEXT_WIDTH = 40; // ширина текста
-var MAX_BAR_HEIGHT = 150;
-var FONT_GAP = 15;
-
-var renderCloud = function(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-};
-
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
-
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
-};
-
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 40 + (GAP * 2));
-
-  var maxTime = getMaxElement(times);
-
-  for (var i = 0; i < players.length; i++) {
-    ctx.fillText(players[i], CLOUD_X + GAP, CLOUD_Y + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
-  }
-};
-
-*/
-
-
-
-/*
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP = 10;
-var FONT_GAP = 15;
-var TEXT_WIDTH = 40;
-var BAR_WIDTH = 40;
-var MAX_BAR_WIDTH = 150;
-var BAR_HEIGHT = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
-
-var renderCloud = function(ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-};
-
-var getMaxElement = function(arr) {
-  var maxElement = arr[0];
-
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
-    }
-  }
-
-  return maxElement;
-};
-
-window.renderStatistics = function(ctx, players, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
-  ctx.font = '16px PT Mono';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-
-  var maxTime = getMaxElement(times);
-
-  // a first result
-  ctx.fillText('2725', 140, 130);
-  ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  ctx.fillRect(140, 140, 50, 100);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Вы', 140, 270);
-
-  // a second result
-  ctx.fillText('4025', 240, 90);
-  ctx.fillStyle = 'hsl(250, 100%, 30%)';
-  ctx.fillRect(240, 100, 50, 140);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Кекс', 240, 270);
-
-  // a third result
-  ctx.fillText('1244', 340, 180);
-  ctx.fillStyle = 'hsl(250, 10%, 50%)';
-  ctx.fillRect(340, 190, 50, 50);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Катя', 340, 270);
-
-  // a fourth result
-  ctx.fillText('1339', 440, 175);
-  ctx.fillStyle = 'hsl(250, 10%, 40%)';
-  ctx.fillRect(440, 185, 50, 55);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Игорь', 440, 270);
-};
-
-*/
